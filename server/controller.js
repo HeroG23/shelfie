280984e7db43eb4d8e825d94f64375339a663c4f
Brian Demro <demrobrian@hotmail.com>
2020-11-06 12:15:23 -0600

    some new stuff added

---
+++ server/controller.js
@@ -0,0 +1,59 @@
+module.export = {
+    createProduct: (req, res) =>{
+        const db = req.app.get('db');
+        const {name, price, img_url} = req.body;
+
+        db.create_product([name, price, img_url])
+        .then (() => res.sendStatus(200))
+        .catch(err => {
+            res.status(500).send(`You can't do that`)
+            console.log(err)
+        });
+    },
+
+    findProduct: (req, res) => {
+        const db = req.app.get('db');
+        const {id} = req.params;
+
+        db.find_product(id)
+        .then(product => res.status(200).send(product))
+        .catch(err => {
+            res.status(500).send(`There's a snake in my boot`)
+            console.log(err)
+        });
+    },
+    findProducts: (req, res) => {
+        const db = req.app.get('db');
+
+        db.find_products()
+        .then(products => res.status(200).send(products))
+        .catch(err => {
+            res.status(500).send(`There's a troll in the dungeon`)
+            console.log(err)
+        });
+    },
+    updateProduct: (req, res) =>{
+        const db = req.app.get('db');
+        const {id} = req.params;
+        const {desc} = req.query;
+
+        db.update_product([id, desc])
+        .then(() => res.sendStatus(200))
+        .catch(err => {
+            res.status(500).send(`Exterminate, exterminate`)
+            console.log(err)
+        });
+    },
+
+    deleteProduct: (req, res) => {
+        const db = req.app.get('db');
+        const {id} = req.params;
+
+        db.delete_product(id)
+        .then(() => res.sendStatus(200))
+        .catch(err => {
+            res.status(500).send(`You're doing it wrong!!`)
+            console.log(err)
+        })
+    }
+}
--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const massive = require('massive');
-// const ctrl = require('./controller.js');
+const ctrl = require('./controller.js');
 const app = express();
 const{SERVER_PORT, CONNECTION_STRING} = process.env;
 
@@ -14,5 +14,11 @@ massive({
 }).catch(err => console.log(err));
 
 app.use(express.json());
+
+app.post('/api/inventory', ctrl.createProduct);
+app.get('/api/inventory', ctrl.findProducts);
+app.get('/api/inventory/:id', ctrl.findProduct);
+app.put('/api/inventory/:id', ctrl.updateProduct);
+app.delete('/api/inventory/:id', ctrl.deleteProduct);
 
 app.listen(SERVER_PORT, () => console.log(`Server listening on port ${SERVER_PORT}`));
